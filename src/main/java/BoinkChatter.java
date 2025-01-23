3f16631d9986592221bd84f10b8f6410543aa736
boinkkitty <boinkitty@gmail.com>
2025-01-24 01:20:17 +0800

    Echo

--- src/main/java/BoinkChatter.java
+++ src/main/java/BoinkChatter.java
@@ -1,8 +1,24 @@
+import java.util.*;
+
 public class BoinkChatter {
+
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
         String logo = "Hello! I'm BoinkChatter \n" +
-                "What can I do for you? \n \n" +
-                "Bye. Hope to see you again soon! \n";
+                "What can I do for you? \n \n";
+
         System.out.println(logo);
+
+        while (true) {
+            String input = scanner.nextLine();
+
+            if(input.equals("bye")) {
+                System.out.println("Bye. Hope to see you again soon! \n");
+                break;
+            } else {
+                System.out.println(input);
+            }
+        }
+
     }
 }
