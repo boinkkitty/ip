473be4ce5e1a2de381fbdad4c6761bc6203ca8f3
boinkkitty <boinkitty@gmail.com>
2025-01-24 00:54:12 +0800

    Rename, Greet, Exit

---
+++ src/main/java/BoinkChatter.java
@@ -0,0 +1,8 @@
+public class BoinkChatter {
+    public static void main(String[] args) {
+        String logo = "Hello! I'm BoinkChatter \n" +
+                "What can I do for you? \n \n" +
+                "Bye. Hope to see you again soon! \n";
+        System.out.println(logo);
+    }
+}
