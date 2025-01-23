53f96d525ea51661878bea2cd595bd1872616af2
boinkkitty <boinkitty@gmail.com>
2025-01-24 01:26:56 +0800

    Add, List

--- src/main/java/BoinkChatter.java
+++ src/main/java/BoinkChatter.java
@@ -4,19 +4,26 @@ public class BoinkChatter {
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
+        ArrayList<String> cmds = new ArrayList<>();
+
         String logo = "Hello! I'm BoinkChatter \n" +
-                "What can I do for you? \n \n";
+                "What can I do for you? \n";
 
         System.out.println(logo);
 
         while (true) {
             String input = scanner.nextLine();
 
-            if(input.equals("bye")) {
+            if (input.equals("bye")) {
                 System.out.println("Bye. Hope to see you again soon! \n");
                 break;
+            } else if (input.equals("list")) {
+                for (int i = 0; i < cmds.size(); ++i) {
+                    System.out.println((i + 1) + ". " + cmds.get(i));
+                }
             } else {
-                System.out.println(input);
+                cmds.add(input);
+                System.out.println("added: " + input);
             }
         }
 
