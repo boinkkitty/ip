9f1af2e55fad2ee52ae42c2fa72d6979ad688ee7
boinkkitty <boinkitty@gmail.com>
2025-01-24 13:11:08 +0800

    Exception Handling

--- src/main/java/BoinkChatter.java
+++ src/main/java/BoinkChatter.java
@@ -1,3 +1,4 @@
+import java.sql.SQLOutput;
 import java.util.*;
 import java.util.regex.*;
 
@@ -22,13 +23,13 @@ public class BoinkChatter {
                 for (int i = 0; i < tasks.size(); ++i) {
                     System.out.println((i + 1) + ". " + tasks.get(i));
                 }
-            } else if (input.contains("unmark")) {
+            } else if (input.startsWith("unmark")) {
                 String[] parts = input.split(" ");
                 int pos = Integer.parseInt(parts[1]) - 1;
                 tasks.get(pos).setNotDone();
                 System.out.println("OK, I've marked this task as not done yet:");
                 System.out.println(tasks.get(pos));
-            } else if (input.contains("mark")) {
+            } else if (input.startsWith("mark")) {
                 String[] parts = input.split(" ");
                 int pos = Integer.parseInt(parts[1]) - 1;
                 tasks.get(pos).setDone();
@@ -36,30 +37,42 @@ public class BoinkChatter {
                 System.out.println(tasks.get(pos));
             } else {
                 Task newTask = createTask(input);
-                tasks.add(newTask);
-                System.out.println("Got it. I've added this task:\n"
-                        + newTask + "\n"
-                        + "Now you have " + tasks.size() + " tasks in the list");
+                if (newTask != null) {
+                    tasks.add(newTask);
+                    System.out.println("Got it. I've added this task:\n"
+                            + newTask + "\n"
+                            + "Now you have " + tasks.size() + " tasks in the list");
+                }
             }
         }
     }
 
     public static Task createTask (String input) {
-        if (input.startsWith("todo")) {
-            String name = input.replaceFirst("todo ","");
-            return new ToDo(name);
-        } else if (input.startsWith("deadline")) {
-            input = input.replaceFirst("deadline ","");
-            String[] parts = input.split("/by ");
-            return new Deadline(parts[0].trim(), parts[1].trim());
-        } else if (input.startsWith("event")) {
-            input = input.replaceFirst("event ","");
-            String[] parts = input.split("/from ");
-            String name = parts[0];
-            String[] secondParts = parts[1].split("/to ");
-            return new Event(name.trim(), secondParts[0].trim(), secondParts[1].trim());
-        } else {
-            return new ToDo("Error");
+        try {
+            String[] check = input.split(" ");
+            if (check.length <= 1) {
+                throw new BoinkException("Can't be empty!");
+            }
+
+            if (input.startsWith("todo")) {
+                String name = input.replaceFirst("todo ","");
+                return new ToDo(name);
+            } else if (input.startsWith("deadline")) {
+                input = input.replaceFirst("deadline ","");
+                String[] parts = input.split("/by ");
+                return new Deadline(parts[0].trim(), parts[1].trim());
+            } else if (input.startsWith("event")) {
+                input = input.replaceFirst("event ","");
+                String[] parts = input.split("/from ");
+                String name = parts[0];
+                String[] secondParts = parts[1].split("/to ");
+                return new Event(name.trim(), secondParts[0].trim(), secondParts[1].trim());
+            } else {
+                throw new BoinkException("Sorry! I don't know what that means :<");
+            }
+        } catch (BoinkException err) {
+            System.out.println(err.getMessage());
+            return null;
         }
     }
 }
---
+++ src/main/java/BoinkException.java
@@ -0,0 +1,10 @@
+public class BoinkException extends Exception {
+    /**
+     * Exceptions thrown by BoinkChatter, chatbot
+     */
+
+    public BoinkException(String msg) {
+        super(msg);
+    }
+
+}
